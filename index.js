220808807110c2763ba40d9651ed92cf1011f7ce
bhavyc <88427302+bhavyc@users.noreply.github.com>
2025-12-07 00:15:44 +0530

    Update CORS origin URL for production

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const app = express();
 // ------------------ CONNECT DATABASE ------------------
 connectDB();
 app.use(cors({
-  origin: "my-shop-dun-nine.vercel.app", 
+  origin: "https://my-shop-dun-nine.vercel.app", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true // Agar cookies/headers use kar raha hai toh zaruri hai
 }));
