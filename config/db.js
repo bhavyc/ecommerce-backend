cc4e730b45df0d39f316c7d5fa7cc95de8057428
bhavyc <88427302+bhavyc@users.noreply.github.com>
2025-12-06 19:21:11 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://bhavyac888_db_user:abcd@cluster0.d3tbrad.mongodb.net/ecommerce?appName=Cluster0");  
+    await mongoose.connect("mongodb+srv://bhavyac888_db_user:abcd@cluster0.d3tbrad.mongodb.net/my_drops?appName=Cluster0");  
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("MongoDB Connection Failed:", err.message);
